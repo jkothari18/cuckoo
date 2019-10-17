fa203da8dd320659bcb185be0854bbae1b1e5c5d
Alan Kan <50220655+alankan2004@users.noreply.github.com>
2019-10-16 21:25:00 -0400

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-## I didn't upload because this file on my computer contains my personal information, please fill in your own after you download it.
-
-EMAIL_ADDRESS = "YOUR EMAIL"
-PASSWORD = "YOUR PASSWORD"
-PHONE_NUMBER = "YOUR PHONE NUMBER"
-
-## For the SCOPES, please read README for more detailed explaination.
-
-SCOPES = ["THE SCOPE YOU ARE USING FOR THE GMAIL API"]
