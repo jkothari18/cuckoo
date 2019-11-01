40fb70ab61afacbc7851f6027e3d55916266c4fd
Alan Kan <alankan2004@gmail.com>
2019-11-01 15:38:08 -0400

    Slight update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from gmailAPI import gmail
 from prasePrice import Price
 from datetime import datetime
+import config
 
 class cuckoo():
     def __init__(self):
@@ -9,10 +10,15 @@ class cuckoo():
 
 
 if __name__ == "__main__":
-
     while True:
         service = gmail().getCreds()
         if float(Price().parsePrice()) > 5:
             print('gotta get that bag @ {}'.format(datetime.now()))
-            gmail().sendMessage(service, "me", gmail().buildMessage('me', '2037510584@txt.att.net', "Price Update", gmail().textMessage()))
+            gmail().sendMessage(service, "me", gmail().buildMessage('me', config.PHONE_NUMBER, "Price Update", gmail().textMessage()))
             time.sleep(20)
+        elif float(Price().parsePrice()) < 3.7:
+            print('Bruhhhhhhh you losing money @ {}'.format(datetime.now()))
+            gmail().sendMessage(service, "me", gmail().buildMessage('me', config.EMAIL_ADDRESS, "Price Update",
+                                                                    gmail().textMessage()))
+            gmail().sendMessage(service, "me", gmail().buildMessage('me', config.PHONE_NUMBER, "Price Update", gmail().textMessage()))
+
